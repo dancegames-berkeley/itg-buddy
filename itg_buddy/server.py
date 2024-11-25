bc141e809f014fc449ad205f90686b1630728cb0
Lucas Clark <lucas.clark@berkeley.edu>
2024-11-24 23:49:52 -0800

    setup pyproject.toml and project.scripts

--- itg_buddy/server.py
+++ itg_buddy/server.py
@@ -9,8 +9,9 @@ def main():
     discord_key = os.getenv("DISCORD_API_KEY")
     if not discord_key or discord_key == "your_discord_api_key_goes_here":
         print("Discord API Key not found.")
-        print("Please set the DISCORD_API_KEY or include it in .env")
+        print("Please set the DISCORD_API_KEY or include it in a .env file")
         exit(1)
+    print(discord_key)
 
 
 if __name__ == "__main__":
