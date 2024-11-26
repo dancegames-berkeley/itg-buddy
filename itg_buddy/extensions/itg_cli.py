195e24760dac1acb1d2099848550bccc9a9fdc07
Lucas Clark <lucas.clark@berkeley.edu>
2024-11-26 15:52:34 -0800

    add add_song message listener and generic reply function

--- itg_buddy/extensions/itg_cli.py
+++ itg_buddy/extensions/itg_cli.py
@@ -41,9 +41,7 @@ class ItgCliCommands(commands.Cog):
             f"{bot.__class__.__name__}.{self.__class__.__name__}"
         )
 
-    @app_commands.command(
-        name="add_pack", description="Add a pack to the machine."
-    )
+    @app_commands.command(description="Add a pack to the machine.")
     @app_commands.describe(link="Link to the pack to add")
     async def add_pack(
         self,
@@ -81,23 +79,38 @@ class ItgCliCommands(commands.Cog):
             f"Added {pack.name} with {num_courses} course(s)."
         )
 
-    @app_commands.command(
-        name="add_song", description="Add a song to the Berkeley Test Bench."
-    )
+    @app_commands.command(description="Add a song to Berkeley Test Bench.")
     @app_commands.describe(link="Link to the song to add")
-    async def add_pack(
+    async def add_song(
         self,
         inter: discord.Interaction,
         link: str,
+    ):
+        await self._add_song_helper(inter, link)
+
+    @commands.Cog.listener()
+    async def on_message(self, msg: discord.Message) -> None:
+        if msg.channel.id != self.add_song_channel_id:
+            return
+        zips = filter(
+            lambda a: a.content_type == "application/zip", msg.attachments
+        )
+        for zip in zips:
+            await self._add_song_helper(msg, zip.url)
+
+    async def _add_song_helper(
+        self, inter_or_msg: discord.Interaction | discord.Message, link: str
     ):
         # Define overwrite handler for add_song
         # TODO: make this more pretty with buttons and stuff
         # Maybe genericize it for both add_song and add_pack?
         # Will need to handle arg type differences
-        def overwrite_handler(
+        async def overwrite_handler(
             _new: tuple[Simfile, str], _old: tuple[Simfile, str]
         ) -> bool:
-            inter.response.send_message("Song already exists. Aborting.")
+            await self.inter_or_msg_reply(
+                inter_or_msg, "Song already exists. Aborting."
+            )
             return False
 
         # Run add_song and handle exceptions accordingly
@@ -113,12 +126,29 @@ class ItgCliCommands(commands.Cog):
             return
         except Exception as e:
             # TODO: maybe convert this into an exception handler for the cog?
-            inter.response.send_message(
-                f"add_song threw an exception: `{type(e)}`\n`{e.args}`"
+            await self.inter_or_msg_reply(
+                inter_or_msg,
+                f"add_song threw an exception: `{type(e)}`\n`{e.args}`",
             )
             return
 
         # Send result message on success
-        inter.response.send_message(
-            f"Added {sm.title} to Berkeley Test Bench."
+        await self.inter_or_msg_reply(
+            inter_or_msg, f"Added {sm.title} to Berkeley Test Bench."
         )
+
+    async def inter_or_msg_reply(
+        self,
+        inter_or_msg: discord.Interaction | discord.Message,
+        content: str,
+        inter_response_kwargs: dict = {},
+        msg_reply_kwargs: dict = {},
+    ):
+        if isinstance(inter_or_msg, discord.Interaction):
+            await inter_or_msg.response.send_message(
+                content, **inter_response_kwargs
+            )
+        elif isinstance(inter_or_msg, discord.Message):
+            await inter_or_msg.reply(content, **msg_reply_kwargs)
+        else:
+            raise ValueError("inter_or_msg is not Interaction or Message")
