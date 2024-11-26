15d5b83af15425ad1b531cbbb7474fb5080bfb71
Lucas Clark <lucas.clark@berkeley.edu>
2024-11-26 00:08:52 -0800

    add_song draft

--- itg_buddy/extensions/itg_cli.py
+++ itg_buddy/extensions/itg_cli.py
@@ -3,12 +3,11 @@ import discord
 import itg_cli
 import logging
 from simfile.dir import SimfilePack
+from simfile.types import Simfile
 from discord.ext import commands
 from discord import app_commands
 from pathlib import Path
 from typing import Optional
-
-discord.TextChannel
 
 
 class ItgCliCommands(commands.Cog):
@@ -54,7 +53,7 @@ class ItgCliCommands(commands.Cog):
         # Define overwrite handler for add_pack
         # TODO: make this more pretty with buttons and stuff
         # Maybe genericize it for both add_song and add_pack?
-        # Will need to handle SimfilePack and Simfile differences
+        # Will need to handle arg type differences
         def overwrite_handler(_new: SimfilePack, _old: SimfilePack) -> bool:
             inter.response.send_message("Pack already exists. Aborting.")
             return False
@@ -66,10 +65,12 @@ class ItgCliCommands(commands.Cog):
                 self.packs,
                 self.courses,
                 overwrite=overwrite_handler,
+                delete_macos_files_flag=True,
             )
         except itg_cli.OverwriteException:
             return
         except Exception as e:
+            # TODO: maybe convert this into an exception handler for the cog?
             inter.response.send_message(
                 f"add_pack threw an exception: `{type(e)}`\n`{e.args}`"
             )
@@ -78,4 +79,46 @@ class ItgCliCommands(commands.Cog):
         # Send result message on success
         inter.response.send_message(
             f"Added {pack.name} with {num_courses} course(s)."
+        )
+
+    @app_commands.command(
+        name="add_song", description="Add a song to the Berkeley Test Bench."
+    )
+    @app_commands.describe(link="Link to the song to add")
+    async def add_pack(
+        self,
+        inter: discord.Interaction,
+        link: str,
+    ):
+        # Define overwrite handler for add_song
+        # TODO: make this more pretty with buttons and stuff
+        # Maybe genericize it for both add_song and add_pack?
+        # Will need to handle arg type differences
+        def overwrite_handler(
+            _new: tuple[Simfile, str], _old: tuple[Simfile, str]
+        ) -> bool:
+            inter.response.send_message("Song already exists. Aborting.")
+            return False
+
+        # Run add_song and handle exceptions accordingly
+        try:
+            sm, _ = itg_cli.add_song(
+                link,
+                self.singles,
+                self.cache,
+                overwrite=overwrite_handler,
+                delete_macos_files_flag=True,
+            )
+        except itg_cli.OverwriteException:
+            return
+        except Exception as e:
+            # TODO: maybe convert this into an exception handler for the cog?
+            inter.response.send_message(
+                f"add_song threw an exception: `{type(e)}`\n`{e.args}`"
+            )
+            return
+
+        # Send result message on success
+        inter.response.send_message(
+            f"Added {sm.title} to Berkeley Test Bench."
         )
