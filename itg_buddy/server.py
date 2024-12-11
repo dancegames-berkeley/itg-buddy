6ae7213c498e90b7128cf70604059f9d20b824b3
Lucas Clark <lucas.clark@berkeley.edu>
2024-12-11 09:43:07 -0800

    merge bot.py with server.py

--- itg_buddy/server.py
+++ itg_buddy/server.py
@@ -1,6 +1,50 @@
 import os
 from dotenv import load_dotenv
-from itg_buddy.bot import ItgBuddy
+import logging
+import discord
+from itg_buddy.extensions.example import ExampleCog
+from itg_buddy.extensions.itg_cli import (
+    ItgCliCog,
+    ItgCliCogConfigError,
+)
+from discord.ext import commands
+
+
+class ItgBuddy(commands.Bot):
+    logger: logging.Logger
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.guild_messages = True
+        super().__init__(
+            *args,
+            **kwargs,
+            intents=intents,
+            command_prefix=commands.when_mentioned,
+        )
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    async def setup_hook(self):
+        # Load ExampleCog
+        await self.add_cog(ExampleCog(self))
+        self.logger.info("Loaded ExampleCog")
+
+        # Load ItgCliCog
+        try:
+            await self.add_cog(ItgCliCog(self))
+            self.logger.info("Loaded ItgCliCog")
+        except ItgCliCogConfigError:
+            self.logger.error(
+                "Aboarted load: missing or invalid ItgCliCog environment variables."
+            )
+
+    async def on_ready(self) -> None:
+        self.logger.info(f"Logged in as {self.user} ({self.user.id})")
 
 
 def main():
@@ -9,7 +53,7 @@ def main():
     discord_key = os.getenv("DISCORD_API_KEY")
     if not discord_key:
         print("Discord API Key not found.")
-        print("Please set the DISCORD_API_KEY or include it in a .env file")
+        print("Please set DISCORD_API_KEY or include it in a .env file")
         exit(1)
 
     bot = ItgBuddy()
