9c105bf424ac256d9cdbcf1108d4e524d8781210
Lucas Clark <laclark2020@gmail.com>
2024-12-15 18:20:52 -0800

    improve exception handling

--- itg_buddy/extensions/itg_cli/itg_cli.py
+++ itg_buddy/extensions/itg_cli/itg_cli.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 import discord
 import itg_cli
 import logging
@@ -69,15 +70,20 @@ class ItgCliCog(commands.Cog):
                 embed=cancelled_embed(), view=None
             )
             return
-        except Exception as e:
-            self.logger.exception(f"add_song threw an exception")
-            await inter.edit_original_response(embed=error_embed(e), view=None)
-            return
 
         # Send result message on success
         embed, file = add_pack_success(pack, inter.user)
         await inter.delete_original_response()
         await inter.channel.send(embed=embed, file=file)
+
+    @add_pack.error
+    async def add_pack_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        self.logger.exception(f"add_pack threw an exception")
+        await ctx.interaction.edit_original_response(
+            embed=error_embed(sys.exc_info()), view=None
+        )
 
     @app_commands.command(description="Add a song to Berkeley Test Bench.")
     @app_commands.describe(link="Link to the song to add")
@@ -90,23 +96,38 @@ class ItgCliCog(commands.Cog):
 
         await self._add_song_helper(inter, link)
 
+    @add_song.error
+    async def add_song_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        self.logger.exception(f"add_song threw an exception")
+        await ctx.interaction.edit_original_response(
+            embed=error_embed(sys.exc_info()), view=None
+        )
+
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message) -> None:
-        if msg.channel.id != self.config.add_song_channel_id:
-            return
-        zips = filter(
-            lambda a: a.content_type == "application/zip", msg.attachments
-        )
-        for zip in zips:
-            self.logger.info(
-                f"{msg.author} executed add_song with link {zip.url}"
+        try:
+            if msg.channel.id != self.config.add_song_channel_id:
+                return
+            zips = filter(
+                lambda a: a.content_type == "application/zip", msg.attachments
             )
-            await self._add_song_helper(msg, zip.url)
+            for zip in zips:
+                self.logger.info(
+                    f"{msg.author} executed add_song with link {zip.url}"
+                )
+                await self._add_song_helper(msg, zip.url)
+        except Exception as e:
+            channel = self.bot.get_channel(self.config.add_song_channel_id)
+            await channel.send(embed=error_embed(e))
+            self.logger.exception("on_message raised an exception")
 
     async def _add_song_helper(
         self, inter_or_msg: discord.Interaction | discord.Message, link: str
     ):
-        # Store the user of the original message and send "thinking" message
+        # Store the author of the original message and send message to be edited
+        # with updates
         if isinstance(inter_or_msg, discord.Interaction):
             user = inter_or_msg.user
             await inter_or_msg.response.defer(thinking=True)
@@ -131,15 +152,8 @@ class ItgCliCog(commands.Cog):
                 inter_or_msg, embed=cancelled_embed(), view=None
             )
             return
-        except Exception as e:
-            self.logger.exception(f"add_song threw an exception")
-            await edit_response(
-                inter_or_msg,
-                embed=error_embed(e),
-            )
-            return
 
-        embed, file = add_song_success(sf, path, inter_or_msg.user)
+        embed, file = add_song_success(sf, path, user)
         channel = inter_or_msg.channel
         # Delete progress message and send success message
         if isinstance(inter_or_msg, discord.Interaction):
